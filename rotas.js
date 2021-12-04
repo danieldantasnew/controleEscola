3a8a5a69e9418d91d5c06d9ac4c07f765cddc99e
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2021-12-04 00:01:03 -0300

    adicionando formulário

--- rotas.js
+++ rotas.js
@@ -10,6 +10,14 @@ rotas.get("/teachers", (req,res)=>{
     return res.render("teachers/index")
 })
 
+rotas.get("/teachers/create", (req, res)=>{
+    return res.render("teachers/create")
+})
+
+rotas.post("/teachers", (req,res)=>{
+    return res.send ("Recebido")
+})
+
 rotas.get("/students", (req,res)=>{
     return res.render("students")
     //a pagina view está configurado em nunjucks.configure("view") lá no servidor então não preciso dizer qual o caminho pois ele irá buscar primeiramente lá
