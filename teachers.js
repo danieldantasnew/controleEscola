5c628c4f6390a30d045cc9889751576a6b672aa5
Daniel Dantas <xxxxxdanielzimmxxxx.danieldant@gmail.com>
2021-12-11 02:22:46 -0300

    ultima etapa do topico

--- teachers.js
+++ teachers.js
@@ -1,4 +1,5 @@
 const fs = require("fs")
+const dados = require("./dados.json")
 
 exports.post = (req,res)=>{
 
@@ -9,7 +10,22 @@ exports.post = (req,res)=>{
             return res.send("Por favor, preencha todos os campos")
     }
 
-    fs.writeFile("dados.json", JSON.stringify(req.body), (err)=>{
+    let{novo_avatar, nome_professor, nascimento, selecionar, aula} = req.body
+
+    nascimento = Date.parse(nascimento)
+    const criado_em = Date.now()
+    const id = Number(dados.teachers.length + 1)
+    dados.teachers.push({
+        id,
+        novo_avatar,
+        nome_professor,
+        nascimento,
+        selecionar,
+        aula,
+        criado_em
+    })
+
+    fs.writeFile("dados.json", JSON.stringify(dados, null, 2), (err)=>{
         if(err) return res.send("Erro na escrita do arquivo")
     })
     
